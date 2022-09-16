e3e8b1e18f9a7b35e3e704a45481028e32c5de75
CarlosRB12 <113734528+CarlosRB12@users.noreply.github.com>
2022-09-16 17:34:05 -0600

    Add files via upload

---
+++ Periodo3ejercicio2.java
@@ -0,0 +1,26 @@
+package Periodo3ejercicio2;
+
+import java.util.Scanner;
+
+
+public class Periodo3ejercicio2 {
+
+
+public static void main(String[] args) {
+Scanner objetoNum = new Scanner (System.in);
+
+int valinicial, valfinal;
+
+valinicial=8;
+System.out.println("Múltiplos de 8 hasta 500");
+
+while(valinicial<=500)
+
+{
+System.out.print (valinicial+ ",");
+valinicial=valinicial+8;
+}
+
+}
+
+}
