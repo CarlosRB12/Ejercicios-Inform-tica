beaf02a61d9c889fd9a71cda8c96d0ecee1ef01f
CarlosRB12 <113734528+CarlosRB12@users.noreply.github.com>
2022-09-16 17:35:13 -0600

    Add files via upload

---
+++ Periodo3ejercicio3.java
@@ -0,0 +1,14 @@
+package Periodo3ejercicio3;
+public class Periodo3ejercicio3 {
+
+
+public static void main(String[] args) {
+int i = 0;
+while (true) { //Condición trivial: siempre cierta
+i++;
+System.out.println ("Valor de i: " +i);
+if (i==9) { break;}
+}
+}
+}
+
